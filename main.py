77b340781620a0177ba1d5367eab57071b18af79
EstebanBreaHell <esteban.breahell@gmail.com>
2025-05-02 23:22:22 +0200

    Sheeeesh

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-# main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Bienvenue dans le chat qui rigole 😸"}
+# Montre le dossier /static pour servir les fichiers (images, CSS, etc.)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Dossier contenant tes templates HTML
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
